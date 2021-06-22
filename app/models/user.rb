3743d1b981fedbe3c01120b2a9fcefbca9824dcd
realvint <victor.malukhin@gmail.com>
2021-06-23 02:03:41 +0300

    google auth fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,6 @@ class User < ApplicationRecord
     data = access_token.info
     user = User.where(email: data['email']).first
 
-    # Uncomment the section below if you want users to be created if they don't exist
     unless user
         user = User.create(
            email: data['email'],
