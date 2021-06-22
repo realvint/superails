d0c1fafa90297f07cd0e3e0078e42a39b8bc1c02
realvint <victor.malukhin@gmail.com>
2021-06-22 23:34:57 +0300

    add bootstrap 5 navbar

--- app/controllers/static_public_controller.rb
+++ app/controllers/static_public_controller.rb
@@ -1,5 +1,6 @@
 class StaticPublicController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[landing_page]
+  # skip_before_action :authenticate_user!, only: %i[landing_page]
+  skip_before_action :authenticate_user!
 
   def landing_page
   end
