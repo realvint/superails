47b82c2131c12522054d0af90953a053f2702ff6
realvint <victor.malukhin@gmail.com>
2021-07-03 14:05:37 +0300

    add github omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2, :github]
 
   def self.from_omniauth(access_token)
     user = User.where(email: access_token.info.email).first
