3948682c0c8320a4aca8bc4e1b52c50cc833dfd5
realvint <victor.malukhin@gmail.com>
2021-07-04 00:28:40 +0300

    CRUD methods for questions_controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   get 'privacy', to: 'static_public#privacy'
   get 'terms', to: 'static_public#terms'
   resources :users, only: %i[index show]
-  resources :questions, only: %i[index]
+  resources :questions
 end
